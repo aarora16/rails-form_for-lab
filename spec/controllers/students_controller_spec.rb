7f0d094ffe1eb982e7d5e5405db1dc4cbc1814cf
aarora16 <a.99.anshul@gmail.com>
2020-10-30 16:02:52 -0500

    Done.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -7,11 +7,13 @@ describe StudentsController do
     context "with valid attributes" do
       it "creates a new student" do
         expect{
+          # student = Student.create(first_name: "Bill", last_name: "Smith")
           post :create, params: { :student => { :first_name => "Bill", :last_name => "Smith" } }
         }.to change(Student,:count).by(1)
       end
 
       it "redirects to the new student" do
+        # student = Student.create(first_name: "Bill", last_name: "Smith")
         post :create, params: { :student => { :first_name => "Bill", :last_name => "Smith" } }
         expect(response).to redirect_to Student.last
       end
